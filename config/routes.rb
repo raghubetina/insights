f3a53f8f38174917d91ef9423d7666abac8d963b
Raghu Betina <rbetina@github.com>
2015-02-20 18:01:36 -0600

    Adds users with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'personas#index'
+
   resources :answers
 
   resources :interviews
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220235413) do
+ActiveRecord::Schema.define(version: 20150221000122) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.text     "insight"
+    t.string   "image"
+    t.integer  "question_id"
+    t.integer  "interview_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "answers", ["interview_id"], name: "index_answers_on_interview_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
+
+  create_table "interviews", force: :cascade do |t|
+    t.date     "held_on"
+    t.string   "name"
+    t.string   "company"
+    t.integer  "user_id"
+    t.integer  "persona_id"
+    t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "interviews", ["persona_id"], name: "index_interviews_on_persona_id"
+  add_index "interviews", ["user_id"], name: "index_interviews_on_user_id"
+
+  create_table "persona_questions", force: :cascade do |t|
+    t.integer  "persona_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "persona_questions", ["persona_id"], name: "index_persona_questions_on_persona_id"
+  add_index "persona_questions", ["question_id"], name: "index_persona_questions_on_question_id"
 
   create_table "personas", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
